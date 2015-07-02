f7bf9baa14e2dd2f1c3a451c10f767af875a5e1e
chase <chase.reckling>
2015-07-02 10:25:04 -0700

    add 'create' functionality and form for skills.

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,4 +6,22 @@ class SkillsController < ApplicationController
   def show
     @skill = Skill.find(params[:id])
   end
+
+  def new
+    @skill = Skill.new()
+  end
+
+  def create
+    @skill = Skill.new(skill_params)
+    if @skill.save()
+      redirect_to skills_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def skill_params
+      params.require(:skill).permit(:name, :description)
+    end
 end
