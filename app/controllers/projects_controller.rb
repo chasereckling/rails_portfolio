0e88b37ae22eaa1afe6efa272235df773387e400
chase <chase.reckling>
2015-07-02 13:03:47 -0700

    allow projects to be added to ther skills page, add CRUD functions to projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,42 @@ class ProjectsController < ApplicationController
       @skill = Skill.find(params[:skill_id])
       @project = @skill.projects.new
     end
+
+  def create
+    @skill = Skill.find(params[:skill_id])
+    @project = @skill.projects.new(project_params)
+    if @project.save
+      redirect_to skill_path(@project.skill)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @skill = Skill.find(params[:skill_id])
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @skill = Skill.find(params[:skill_id])
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = "Project successfully updated!"
+      redirect_to skill_path(@project.skill)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project successfully deleted."
+    redirect_to skill_path(@project.skill)
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :link, :description)
+  end
 end
