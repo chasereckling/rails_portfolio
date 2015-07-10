4d94523265a2d59dc55413a2f679d3f34f8896a9
chase <chase.reckling>
2015-07-10 16:11:54 -0700

    add update and delete to blogs.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,25 @@ class BlogsController < ApplicationController
     end
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+
   private
     def blog_params
       params.require(:blog).permit(:title, :body)
