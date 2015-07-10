e23f986175b7e28dbd4143f406c9441f2cbb11f0
chase <chase.reckling>
2015-07-10 16:40:36 -0700

    add user association in comment and blog model.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,7 @@
 class Blog < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
   validates :title, :presence => true
   validates :body, :presence => true
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :blog
+  belongs_to :user
   validates :author, :presence => true
   validates :body, :presence => true
 end
