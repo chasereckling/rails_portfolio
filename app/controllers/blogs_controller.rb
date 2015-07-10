afad5f5b8ae4c173a0f8683511c8a610baa6a41d
chase <chase.reckling>
2015-07-10 11:25:29 -0700

    create blog post and display on blogs index page all blog posts, style navbar to have CRUD functions at thr far right end.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,4 +6,18 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new()
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save()
+      redirect_to blogs_path
+    else
+      render :new
+    end
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
 end
